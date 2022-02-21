dc0f353f13ee554e4694852a95bd79b0b955c1fd
AntonovSergey12 <sergeyantonov2011@gmail.com>
2022-02-21 21:05:46 +0200

    added
    1) сделал стилизацию и позиционирование второй страницы
    3d-print

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".section-slider__wrapper", {
+    loop: true,
+    slidesPerView: 2,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        slidesPerView: 'auto',
+      },
+   
+}) 
